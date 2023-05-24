7d93bba089e2e74fa47169e38de7379904ed1e45
kvs2022 <114330487+kvs2022@users.noreply.github.com>
2023-05-24 22:22:18 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,20 @@
+def no(lt):
+    for i in range(len(lt)):
+        for j in range(i+1,len(lt)):
+
+                if lt[i]>lt[j]:
+                    temp=lt[i]
+                    lt[i]=lt[j]
+                    lt[j]=temp
+            
+    return lt
+
+n=int(input('Enter the total number of elements = '))
+l=[]
+for j in range(n):
+    ele=eval(input('Enter a number = '))
+    l.append(ele)
+lt1=no(l)
+print("Sorted numbers = ")
+for x in range(len(lt1)):
+    print(lt1[x],end=',')
